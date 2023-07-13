c87766558548dbf8c627d493f4b124143f7cc6d2
Andrew Bradley <cspotcode@gmail.com>
2023-07-12 23:15:28 -0400

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,3 +1,27 @@
-self.addEventListener("fetch", function(event) {
-  console.log(`start server worker`)
+const networkFirst = (event) => {
+    event.respondWith(
+      fetch(event.request)
+        .then((networkResponse) => {
+          return caches.open(currentCache).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          })
+        })
+        .catch(() => {
+           return caches.match(event.request);
+        })
+    );
+    };
+self.addEventListener("fetch", event => {
+  networkFirst(event);
+});
+const currentCache = 'v1';
+self.addEventListener('activate', event => {
+ event.waitUntil(
+   caches.keys().then(cacheNames => Promise.all(
+     cacheNames
+       .filter(cacheName => cacheName !== currentCache)
+       .map(cacheName => caches.delete(cacheName))
+   ))
+ );
 });
